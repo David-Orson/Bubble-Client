c044b2211109013a12808a0b00ac1cc03ed4a350
David Orson <onlyorson@outlook.com>
2020-07-27 07:01:01 +0100

    REST Based React Components

--- src/components/streams/StreamCreate.js
+++ src/components/streams/StreamCreate.js
@@ -1,63 +1,21 @@
 import React from "react";
-import { Field, reduxForm } from "redux-form";
+import { connect } from "react-redux";
+import { createStream } from "../../Redux/actions";
+import StreamForm from "./StreamForm";
 
 class StreamCreate extends React.Component {
-  renderError({ error, touched }) {
-    if (touched && error) {
-      return (
-        <div className="ui error message">
-          <div className="header">{error}</div>
-        </div>
-      );
-    }
-  }
-
-  renderInput = ({ input, label, meta }) => {
-    const className = `field ${meta.error && meta.touched ? "error" : ""}`;
-    return (
-      <div className={className}>
-        <label>{label}</label>
-        <input {...input} autoComplete="off" />
-        {this.renderError(meta)}
-      </div>
-    );
+  onSubmit = (formValues) => {
+    this.props.createStream(formValues);
   };
-
-  onSubmit(formValues) {}
 
   render() {
     return (
-      <form
-        onSubmit={this.props.handleSubmit(this.onSubmit)}
-        className="ui form error"
-      >
-        <Field label="Enter Title" name="title" component={this.renderInput} />
-        <Field
-          label="Enter Description"
-          name="description"
-          component={this.renderInput}
-        />
-        <button className="ui button primary">Submit</button>
-      </form>
+      <div>
+        <h3>Create a Stream</h3>
+        <StreamForm onSubmit={this.onSubmit} />
+      </div>
     );
   }
 }
 
-const validate = (formValues) => {
-  const errors = {};
-
-  if (!formValues.title) {
-    errors.title = "Please enter a Title";
-  }
-
-  if (!formValues.description) {
-    errors.description = "Please enter a description";
-  }
-
-  return errors;
-};
-
-export default reduxForm({
-  form: "streamCreate",
-  validate,
-})(StreamCreate);
+export default connect(null, { createStream })(StreamCreate);
